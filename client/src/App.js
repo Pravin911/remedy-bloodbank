0384bd89f2b981e1f59a460e6b544c877a3ca14e
Pravin .S. Gadai <gadai1886pravin@gmail.com>
2024-02-17 12:33:45 +0530

    install tailwindcss and antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import { Button } from "antd";
 
 function App() {
   return (
-    <div>
-      <h1>Remedy Blood Bank</h1>
+    <div className="flex h-screen justify-center items-center bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">Remedy Blood Bank</h1>
     </div>
   );
 }
